8ee852aaeb8371899da9b6df29bca4f5073ac31f
tomas <kmettom@gmail.com>
2021-10-25 09:32:42 +0200

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const DeclarationBundlerPlugin = require('declaration-bundler-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 var config = {
   entry: './src/index.ts',
@@ -27,6 +28,12 @@ var config = {
     libraryTarget: 'commonjs',
   },
   plugins: [
+    new ESLintPlugin(
+      {
+        extensions: ['.tsx', '.ts', '.js', 'json'],
+
+      }
+    ),
   // new CleanWebpackPlugin(['./@types', './dist']),
   // new DeclarationBundlerPlugin({
   //   moduleName: '"@mycomp/mylib"',
