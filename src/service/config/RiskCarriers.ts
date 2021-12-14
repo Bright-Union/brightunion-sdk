25d62ba261feaf884e876644cba7f8dfb16165e2
tomas <kmettom@gmail.com>
2021-12-14 17:49:42 +0100

    small code clean - RiskCarriers

--- src/service/config/RiskCarriers.ts
+++ src/service/config/RiskCarriers.ts
@@ -1,8 +1,3 @@
-// const alchemix = require('@/assets/img/alchemix.webp');
-// const keeper = require('@/assets/img/keeperDAO.webp');
-// const universe = require('@/assets/img/universexyz.webp');
-// const zeroExchange = require('@/assets/img/zeroExchange.webp');
-// const anchor = require('@/assets/img/anchor.webp');
 
 const NEXUS:any = {
     description: {
@@ -112,72 +107,6 @@ const STAKE_NOTES:object = {
     }
 }
 
-const CUSTOM_BRIDGE_PROTOCOLS:object = {
-    '0xF0939011a9bb95c3B791f0cb546377Ed2693a574': {
-        // logoURI: zeroExchange,
-        name: '0.exchange'
-    },
-    '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9': {
-        // logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9/logo.png',
-        name: 'Aave'
-    },
-    '0x0000000000000000000000000000000000000001': {
-        // logoURI: anchor,
-        name: 'Anchor Protocol'
-    },
-    '0xdBdb4d16EdA451D0503b854CF79D55697F90c8DF': {
-        // logoURI: alchemix,
-        name: 'Alchemix'
-    },
-    '0xa1faa113cbE53436Df28FF0aEe54275c13B40975': {
-        // logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xa1faa113cbE53436Df28FF0aEe54275c13B40975/logo.png',
-        name: 'Alpha Token'
-    },
-    '0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C': {
-        // logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C/logo.png',
-        name: 'Bancor'
-    },
-    '0x0391D2021f89DC339F60Fff84546EA23E337750f': {
-        // logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x0391D2021f89DC339F60Fff84546EA23E337750f/logo.png',
-        name: 'BarnBridge'
-    },
-    '0x3472A5A71965499acd81997a54BBA8D852C6E53d': {
-        // logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x3472A5A71965499acd81997a54BBA8D852C6E53d/logo.png',
-        name: 'BadgerDAO'
-    },
-    '0xf16e81dce15B08F326220742020379B855B87DF9': {
-        // logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xf16e81dce15B08F326220742020379B855B87DF9/logo.png',
-        name: 'IceToken'
-    },
-    '0x903bef1736cddf2a537176cf3c64579c3867a881': {
-        // logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x903bef1736cddf2a537176cf3c64579c3867a881/logo.png',
-        name: 'ICHI'
-    },
-    '0xfA5047c9c78B8877af97BDcb85Db743fD7313d4a': {
-        // logoURI: keeper,
-        name: 'Keeper DAO'
-    },
-    '0x808507121b80c02388fad14726482e061b8da827': {
-        // logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x808507121b80c02388fad14726482e061b8da827/logo.png',
-        name: 'Pendle'
-    },
-    '0x6B3595068778DD592e39A122f4f5a5cF09C90fE2': {
-        // logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B3595068778DD592e39A122f4f5a5cF09C90fE2/logo.png',
-        name: 'Sushi'
-    },
-    '0x618679dF9EfCd19694BB1daa8D00718Eacfa2883': {
-        // logoURI: universe,
-        name: 'Universe.XYZ'
-    },
-    '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984': {
-        // logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984/logo.png',
-        name: 'Uniswap'
-    },
-    '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e': {
-        // logoURI: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e/logo.png',
-        name: 'Yearn Finance (all vaults)'
-    },
-}
 
 const PROTOCOL_TYPES:object = {
     protocol: 'In case the user has financial loss due to failure of the protocol caused by code, governance and oracle failures',
@@ -191,5 +120,3 @@ export default class RiskCarriers {
   public static NEXUS = NEXUS;
   public static BRIDGE = BRIDGE;
 }
-
-// export default { CUSTOM_BRIDGE_PROTOCOLS, NEXUS, BRIDGE, INSURACE }
