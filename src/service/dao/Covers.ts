01c5beac65adbb133dbb92448faecd1b934b9dc6
tomas <kmettom@gmail.com>
2022-02-14 17:19:09 +0000

    clean get covers code

--- src/service/dao/Covers.ts
+++ src/service/dao/Covers.ts
@@ -182,34 +182,9 @@ export async function getCoversInsurace(_web3:any):Promise<any>{
 
   let allCovers:any = [];
   const insuraceCoverInstance = await  _getInsuraceDistributor(NetConfig.netById(_web3.networkId).insuraceCover, _web3.web3Instance);
-  // if(_web3.symbol == 'AVALANCHE'){
-  //   console.log('1a' ,_web3.symbol,  insuraceCoverInstance);
-  // }
-
   const coverDataAddress = await insuraceCoverInstance.methods.data().call()
-  .then((_data: any) => {
-    console.log("coverDataAddress" , _web3.symbol , _data);
-    return _data;
-  }).catch((e:any) => {
-    return {error: "coverDataAddress not available at" + _web3.symbol }
-  })
-
-  if(coverDataAddress.error){
-    return allCovers;
-  }
-
   const coverDataInstance = await _getInsurAceCoverDataContract(coverDataAddress, _web3.web3Instance);
-
-  // console.log('3' ,_web3.symbol,  coverDataInstance);
-
   const count =  await coverDataInstance.methods.getCoverCount(_web3.account).call();
-
-  console.log('4' ,_web3.symbol,  count);
-
-  // if(_web3.symbol == 'AVALANCHE'){
-  //   console.log('4' ,_web3.symbol,  count);
-  // }
-
 
   for (let coverId = 1; coverId <= Number(count); coverId++) {
 
