42cb1b5fd45a8adcbfdfb9caaf1e7780ccb44988
Ann Dvorkina <anndvorkina@gmail.com>
2022-03-21 20:20:05 +0300

    Track transaction hash

--- src/service/helpers/ERC20Helper.ts
+++ src/service/helpers/ERC20Helper.ts
@@ -10,11 +10,12 @@ export default class ERC20Helper {
         return toBN(ercAmount).div(toBN(10 ** 12)).toString();
     }
 
-    static approveAndCall ( erc20Instance:any, spender:any, amount:any, onConfirmation:any, onError:any) {
+    static approveAndCall ( erc20Instance:any, spender:any, amount:any, onTxHash:any, onConfirmation:any, onError:any) {
         return erc20Instance.methods
             .approve(spender, amount)
             .send({from: global.user.account})
             .on('transactionHash', () => {
+                onTxHash();
                 //
             })
             .on('confirmation', (confirmationNumber:any) => {
@@ -29,7 +30,7 @@ export default class ERC20Helper {
 
     //  @dev USDT (unfortunately) doesn't allow to change the allowance, let say from X to Y
     //  Instead, we have to set it to 0 first, and then set it to Y
-    static approveUSDTAndCall (erc20Instance:any, spender:any, amount:any, onAllowanceReset:any, onConfirmation:any, onError:any) {
+    static approveUSDTAndCall (erc20Instance:any, spender:any, amount:any, onAllowanceReset:any, onTxHash:any, onConfirmation:any, onError:any) {
 
         erc20Instance.methods.allowance(global.user.account, spender).call().then((currentAllowance:any) => {
 
@@ -46,11 +47,11 @@ export default class ERC20Helper {
                     .on('confirmation', (confirmationNumber:any) => {
                         if (confirmationNumber === 0) {
                             //starting over again
-                            ERC20Helper.approveAndCall(erc20Instance, spender, ERC20Helper.ERCtoUSDTDecimals(amount), onConfirmation, onError);
+                            ERC20Helper.approveAndCall(erc20Instance, spender, ERC20Helper.ERCtoUSDTDecimals(amount), onTxHash, onConfirmation, onError);
                         }
                     })
             } else {
-                ERC20Helper.approveAndCall(erc20Instance, spender, ERC20Helper.ERCtoUSDTDecimals(amount), onConfirmation, onError);
+                ERC20Helper.approveAndCall(erc20Instance, spender, ERC20Helper.ERCtoUSDTDecimals(amount), onTxHash, onConfirmation, onError);
             }
         });
     }
