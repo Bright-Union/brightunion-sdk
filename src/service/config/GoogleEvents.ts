1e1514081de38fe07754cf8bc0bdd098336ed6eb
tomas <kmettom@gmail.com>
2022-04-21 11:49:33 +0100

    fix Init function - Release 1.2.52

--- src/service/config/GoogleEvents.ts
+++ src/service/config/GoogleEvents.ts
@@ -37,6 +37,16 @@ const CURRENCIES: any = {
 
 class GoogleEvents {
 
+  static InitTechManager = () => {
+
+    // (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    // new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    // j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    // 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    // })(window,document,'script','dataLayer','GTM-WCCMKXR');
+
+  }
+
   static onBUInit = () => {
     if(NetConfig.isMainNetwork(global.user.networkId) && global.user.googleEventsEnabled ){
 
