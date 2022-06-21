e0293b399d8b09328d05e0aa1db73aa8969b3300
Ann Dvorkina <anndvorkina@gmail.com>
2022-06-21 14:25:27 +0300

    Rollover date added

--- src/service/distributorsApi/UnslashedAPI.ts
+++ src/service/distributorsApi/UnslashedAPI.ts
@@ -39,6 +39,10 @@ export default class UnslashedAPI {
                 let apy = await unslashedInstance.methods.getDynamicPricePerYear18eRatio().call().then((pricePerYear:any) => {
                     return fromWei(pricePerYear);
                 })
+                let rolloverDate = await unslashedInstance.methods.getRolloverDate().call().then((timestamp:any) => {
+                  return timestamp;
+                })
+
                 let price = await unslashedInstance.methods.coverToPremium(toWei(String(coveredAmount))).call().then((premium:any) => {
                   return premium;
                 })
@@ -56,6 +60,7 @@ export default class UnslashedAPI {
                         chain: 'ETH',
                         name: quote.cover.name,
                         source: 'ease',
+                        actualPeriod: rolloverDate,
                         rawDataUnslashed: quote.cover.static,
                         type: quote.cover.type,
                         typeDescription: quote.cover.static.description,
@@ -66,6 +71,7 @@ export default class UnslashedAPI {
                         protocol,
                         {
                             amount: amount,
+                            actualPeriod: rolloverDate,
                             currency: currency,
                             period: period,
                             chain: 'ETH',
