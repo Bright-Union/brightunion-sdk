6fb2c05b990a87fe9b7e673511d80170dac8c949
kivanov82 <kivanov82@yahoo.com>
2023-05-17 11:12:46 +0200

    Bug fix for Uniswap API

--- src/service/helpers/UniswapV2Api.ts
+++ src/service/helpers/UniswapV2Api.ts
@@ -1,12 +1,16 @@
+import { ethers } from 'ethers'
 import {Fetcher, Route, Token, WETH} from '@uniswap/sdk'
+
+import NetConfig from '../../service/config/NetConfig'
 
 class UniswapV2Api {
 
   public  static priceTokenAtoETH (chainId:any, tokenA:any) {
         const TokenA = new Token(Number(chainId), tokenA, 18);
         const weth:any = WETH;
+        const provider = new ethers.providers.JsonRpcProvider( NetConfig.getQuickNodeProvider() );
 
-        return Fetcher.fetchPairData(TokenA, weth[chainId] ).then((pair:any) => {
+        return Fetcher.fetchPairData(TokenA, weth[chainId], provider).then((pair:any) => {
             const route = new Route( [pair] , weth[chainId] )
             return route.midPrice.toSignificant(6);
         })
@@ -14,8 +18,9 @@ class UniswapV2Api {
   public  static priceETHtoTokenB (chainId:any, tokenB:any) {
         const TokenB = new Token(Number(chainId), tokenB, 18);
         const weth:any = WETH;
+        const provider = new ethers.providers.JsonRpcProvider( NetConfig.getQuickNodeProvider() );
 
-        return Fetcher.fetchPairData( TokenB , weth[chainId] ).then((pair:any) => {
+        return Fetcher.fetchPairData( TokenB , weth[chainId], provider).then((pair:any) => {
             const route = new Route( [pair] , weth[chainId] )
             return route.midPrice.toSignificant(6);
         })
@@ -24,8 +29,9 @@ class UniswapV2Api {
   public  static priceTokenAtoTokenB (chainId:any, tokenA:any, tokenB:any) {
         const TokenA = new Token(Number(chainId), tokenA, 18);
         const TokenB = new Token(Number(chainId), tokenB, 18);
+        const provider = new ethers.providers.JsonRpcProvider( NetConfig.getQuickNodeProvider() );
 
-        return Fetcher.fetchPairData(TokenA, TokenB).then((pair:any) => {
+        return Fetcher.fetchPairData(TokenA, TokenB, provider).then((pair:any) => {
           const route = new Route([pair], TokenB)
           return route.midPrice.toSignificant(6);
         })
